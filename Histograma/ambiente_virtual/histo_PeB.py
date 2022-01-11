373d7220027ee222a1c83f3a26a38f2e75007ecd
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-10 23:51:09 -0300

    ajustando alguns comentários

--- Histograma/ambiente_virtual/histo_PeB.py
+++ Histograma/ambiente_virtual/histo_PeB.py
@@ -7,9 +7,9 @@ from matplotlib import pyplot as plt
 # Carrega a imágem e preto e branco
 img = cv2.imread("img.jpg",0)
 
+#parametros de .hist
 #ravel -> pega matriz 2d (que é imagem) e tranforma em um vetor de uma dimenção
-#bins -> quanto menos bins mais tons tem em um ponto do eixo x do histograma
-#256 garante que cada bin tem um ton
+#bins -> quanto menos bins mais tons tem em um ponto do eixo x do histograma (256 garante que cada bin tem um ton)
 #intervalo do histograma
 plt.hist(img.ravel(), 256, [0, 256])
 
